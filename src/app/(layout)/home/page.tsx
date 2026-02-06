8e0e20f6211ab851f34f8dc504c9213319e84d85
guddukumar <gk4communication@gmail.com>
2026-02-06 19:01:48 +0530

    Deploy Next.js app router

--- src/app/(layout)/home/page.tsx
+++ src/app/(layout)/home/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="min-h-screen flex items-center justify-center">
       <div className="max-w-4xl text-center">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold tracking-tight">
           Hi, I’m <span className="text-primary">GUDDU KUMAR</span> 👋
         </h1>
 
